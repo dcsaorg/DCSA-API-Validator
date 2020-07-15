a8bbe006d3a6be360c1937aa5f97b8e254132d3b
Nicolas Guilbert <nicolas@ange.dk>
2020-07-15 09:41:02 +0200

    Minimalistic initial test

---
+++ README.md
@@ -0,0 +1,4 @@
+The DCSA API Validator
+======================
+
+Given a target publisher of DCSA's OpenAPI, the validator tests that all end points are behaving as expected.
---
+++ src/test/java/org/dcsa/api-validator/RestAssuredSimpleTest.java
@@ -0,0 +1,17 @@
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+import org.testng.annotations.Test;
+
+/*
+ * A simple test illustrating the use of REST Assured
+ */
+
+public class RestAssuredSimpleTest {
+
+    final static String ROOT_URI = "http://localhost:9090";
+
+    @Test
+    public void simple_get_test() {
+        get(ROOT_URI + "/events").then().assertThat().body("size()", greaterThanOrEqualTo(0));
+    }
+}

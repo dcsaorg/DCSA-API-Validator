a14acc30c9f57908e4f5276b708834f958ac5abd
Asger Stage <as@asseco.dk>
2020-08-05 09:12:03 +0200

    Update eventsubscription tests

--- src/test/java/org/dcsa/api_validator/eventSubscriptionsTest.java
+++ src/test/java/org/dcsa/api_validator/eventSubscriptionsTest.java
@@ -6,9 +6,8 @@ import org.everit.json.schema.loader.SchemaLoader;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONTokener;
-import org.junit.Assert;
-import org.junit.jupiter.api.BeforeAll;
-import org.testng.annotations.Test;
+import org.testng.Assert;
+import org.testng.annotations.*;
 import spark.Request;
 import spark.Spark;
 
@@ -20,14 +19,23 @@ import java.util.concurrent.TimeUnit;
 import static io.restassured.RestAssured.given;
 
 public class eventSubscriptionsTest {
-    private static final CountDownLatch lock = new CountDownLatch(1); //Initialize countdown at 1, when count is 0 lock is released
+    //Don't reuse request objects to reduce risk of other unrelated events affecting the tests
     private static Request req;
+    private static Request reqTransportEvent;
+    private static CountDownLatch lock;
 
-    @BeforeAll
-    public static void initServer() {
+    @BeforeTest
+    static void setup() {
+        lock = new CountDownLatch(1); //Initialize countdown at 1, when count is 0 lock is released
+
         Spark.port(4567);
         Spark.post("/webhook/receive", (req, res) -> {
             eventSubscriptionsTest.req = req;
+            lock.countDown(); //Release lock
+            return "Callback received!";
+        });
+        Spark.post("/webhook/receive-transport-events", (req, res) -> {
+            eventSubscriptionsTest.reqTransportEvent = req;
             lock.countDown(); //Release lock
             return "Callback received!";
         });
@@ -35,10 +43,15 @@ public class eventSubscriptionsTest {
 
     }
 
+    @AfterTest
+    static void cleanUp() {
+        req=null;
+        reqTransportEvent=null;
+        Spark.awaitStop();
+    }
+
     @Test
     public void testCallbacks() throws InterruptedException, IOException, JSONException {
-
-        initServer();
         given().
                 contentType("application/json").
                 body("{\n" +
@@ -51,7 +64,8 @@ public class eventSubscriptionsTest {
                 post(Configuration.ROOT_URI + "/events");
 
         lock.await(20000, TimeUnit.MILLISECONDS); //Released immediately if lock countdown is 0
-        Assert.assertNotNull( req.body());
+        Assert.assertNotNull(req);
+        Assert.assertNotNull(req.body());
 
         //Validate that the callback body is a Shipment Event
         JSONObject jsonSubject = new JSONObject(req.body());
@@ -62,6 +76,23 @@ public class eventSubscriptionsTest {
             schema.validate(jsonSubject); // throws a ValidationException if this object is invalid
         }
 
+    }
+
+    @Test
+    public void testCallbackFilter() throws InterruptedException, JSONException {
+        given().
+                contentType("application/json").
+                body("{\n" +
+                        "            \"eventDateTime\": \"2020-07-14T22:00:00.000+00:00\"," +
+                        "            \"eventClassifierCode\": \"PLN\",\n" +
+                        "            \"eventType\": \"SHIPMENT\"," +
+                        "            \"eventTypeCode\": \"DEPA\"," +
+                        "            \"shipmentInformationTypeCode\": \"Asger text\"" +
+                        "        }").
+                post(Configuration.ROOT_URI + "/events");
+
+        lock.await(10000, TimeUnit.MILLISECONDS); //Released immediately if lock countdown is 0
+        Assert.assertNull(reqTransportEvent); //The body should be null, since only transport events must be sent to this endpoint
 
     }
 

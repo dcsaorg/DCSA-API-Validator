666c0aebe29b0efeac6e5095ccf2edf6d2f59e62
Asger Stage <as@asseco.dk>
2020-09-21 11:30:02 +0200

    Version the validator
    Test suites are now placed in versioned packages, that can be run using
    a test.suite parameter. For example:
    mvn -Dtest.suite=TnTV1.xml test
    Will run the TnT v1 test suite. Multiple suits can be run at once
    by comma-separating the parameters, for example:
    mvn -Dtest.suite=TnTV1.xml,TnTV2.xml test
    
    Other:
    * Rename workflow to be specific to master branch
    * Add workflow for v1 branch
    
    A push to API validator will run tests on master and v1 branch currently.

---
+++ src/test/java/org/dcsa/api_validator/tnt/v1/RestAssuredSimpleTest.java
@@ -0,0 +1,31 @@
+package org.dcsa.api_validator.tnt.v1;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+import org.dcsa.api_validator.conf.Configuration;
+import org.testng.annotations.Test;
+
+/*
+ * A simple test illustrating the use of REST Assured
+ */
+
+public class RestAssuredSimpleTest {
+
+
+    @Test
+    public void simple_get_test() {
+        given().
+                auth().
+                oauth2(Configuration.accessToken).
+                get(Configuration.ROOT_URI + "/events").then().assertThat().body("size()", greaterThanOrEqualTo(0));
+    }
+
+    @Test
+    public void connectionSucceded() {
+        given().
+                auth().
+                oauth2(Configuration.accessToken).
+                get(Configuration.ROOT_URI + "/events").then().assertThat().statusCode(200);
+    }
+}

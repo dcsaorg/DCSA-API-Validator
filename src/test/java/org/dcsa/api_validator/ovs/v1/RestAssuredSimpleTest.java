4ee7e304d8c74e167cbb277ef1abce3111a5ab38
Asger Stage <as@asseco.dk>
2020-09-23 14:00:42 +0200

    Add OVS suite

---
+++ src/test/java/org/dcsa/api_validator/ovs/v1/RestAssuredSimpleTest.java
@@ -0,0 +1,31 @@
+package org.dcsa.api_validator.ovs.v1;
+
+import org.dcsa.api_validator.conf.Configuration;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+
+/*
+ * A simple test illustrating the use of REST Assured
+ */
+
+public class RestAssuredSimpleTest {
+
+
+    @Test
+    public void simple_get_test() {
+        given().
+                auth().
+                oauth2(Configuration.accessToken).
+                get(Configuration.ROOT_URI + "/schedules").then().assertThat().body("size()", greaterThanOrEqualTo(0));
+    }
+
+    @Test
+    public void connectionSucceded() {
+        given().
+                auth().
+                oauth2(Configuration.accessToken).
+                get(Configuration.ROOT_URI + "/schedules").then().assertThat().statusCode(200);
+    }
+}

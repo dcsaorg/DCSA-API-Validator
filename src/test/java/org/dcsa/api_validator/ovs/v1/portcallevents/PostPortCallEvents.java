be0ea243c057361ae9b5ba29149e63067bc4443e
holmHin <holm.hinrichs@t-online.de>
2021-02-10 12:38:09 +0100

    Added Post_Tests for TransportCalls and PortCallEvents

---
+++ src/test/java/org/dcsa/api_validator/ovs/v1/portcallevents/PostPortCallEvents.java
@@ -0,0 +1,252 @@
+package org.dcsa.api_validator.ovs.v1.portcallevents;
+
+import org.dcsa.api_validator.conf.Configuration;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasKey;
+
+public class PostPortCallEvents {
+
+    @Test
+    public void testPostNewETABerth(){
+        given().
+                auth().
+                oauth2(Configuration.accessToken).
+                contentType("application/json").
+                body(   "  {\n" +
+                        "  \"eventType\": \"PORTCALL\",\n" +
+                        "    \"eventDateTime\": \"2021-01-30T22:00:00+02:00\",\n" +
+                        "    \"eventClassifierCode\": \"EST\",\n" +
+                        "    \"eventTypeCode\": \"ARRI\",\n" +
+                        "    \"transportCallID\": \""+this.queryForTransportCallID()+"\",\n" +
+                        "    \"locationType\": \"BERTH\",\n" +
+                        "    \"locationID\": \"Meter 100-300\",\n" +
+                        "    \"comment\": null,\n" +
+                        "    \"delayReasonCode\": null" +
+                        "}\n").
+                when().post(Configuration.ROOT_URI + "/transport-calls/port-call-events").
+                then().body("$", hasKey("eventID"));
+    }
+
+    @Test
+    public void testPostNewRTABerth(){
+        given().
+                auth().
+                oauth2(Configuration.accessToken).
+                contentType("application/json").
+                body(   "  {\n" +
+                        "  \"eventType\": \"PORTCALL\",\n" +
+                        "    \"eventDateTime\": \"2021-01-30T22:00:00+02:00\",\n" +
+                        "    \"eventClassifierCode\": \"REQ\",\n" +
+                        "    \"eventTypeCode\": \"ARRI\",\n" +
+                        "    \"transportCallID\": \""+this.queryForTransportCallID()+"\",\n" +
+                        "    \"locationType\": \"BERTH\",\n" +
+                        "    \"locationID\": \"Meter 100-300\",\n" +
+                        "    \"comment\": \"Delay due to bad Weather!\",\n" +
+                        "    \"delayReasonCode\": \"WEA\"" +
+                        "}\n").
+                when().post(Configuration.ROOT_URI + "/transport-calls/port-call-events").
+                then().body("$", hasKey("eventID"));
+    }
+
+    @Test
+    public void testPostNewPTABerth(){
+        given().
+                auth().
+                oauth2(Configuration.accessToken).
+                contentType("application/json").
+                body(   "  {\n" +
+                        "  \"eventType\": \"PORTCALL\",\n" +
+                        "    \"eventDateTime\": \"2021-01-30T22:00:00+02:00\",\n" +
+                        "    \"eventClassifierCode\": \"PLN\",\n" +
+                        "    \"eventTypeCode\": \"ARRI\",\n" +
+                        "    \"transportCallID\": \""+this.queryForTransportCallID()+"\",\n" +
+                        "    \"locationType\": \"BERTH\",\n" +
+                        "    \"locationID\": \"Meter 100-300\",\n" +
+                        "    \"comment\": null,\n" +
+                        "    \"delayReasonCode\": null" +
+                        "}\n").
+                when().post(Configuration.ROOT_URI + "/transport-calls/port-call-events").
+                then().body("$", hasKey("eventID"));
+    }
+
+
+    @Test
+    public void testPostNewETAPBP(){
+        given().
+                auth().
+                oauth2(Configuration.accessToken).
+                contentType("application/json").
+                body(   "  {\n" +
+                        "  \"eventType\": \"PORTCALL\",\n" +
+                        "    \"eventDateTime\": \"2021-01-30T20:20:00+02:00\",\n" +
+                        "    \"eventClassifierCode\": \"EST\",\n" +
+                        "    \"eventTypeCode\": \"ARRI\",\n" +
+                        "    \"transportCallID\": \""+this.queryForTransportCallID()+"\",\n" +
+                        "    \"locationType\": \"PBP\",\n" +
+                        "    \"locationID\": \"Meter 100-300\",\n" +
+                        "    \"comment\": null,\n" +
+                        "    \"delayReasonCode\": null" +
+                        "}\n").
+                when().post(Configuration.ROOT_URI + "/transport-calls/port-call-events").
+                then().body("$", hasKey("eventID"));
+    }
+
+    @Test
+    public void testPostNewRTAPBP(){
+        given().
+                auth().
+                oauth2(Configuration.accessToken).
+                contentType("application/json").
+                body(   "  {\n" +
+                        "  \"eventType\": \"PORTCALL\",\n" +
+                        "    \"eventDateTime\": \"2021-01-30T20:20:00+02:00\",\n" +
+                        "    \"eventClassifierCode\": \"REQ\",\n" +
+                        "    \"eventTypeCode\": \"ARRI\",\n" +
+                        "    \"transportCallID\": \""+this.queryForTransportCallID()+"\",\n" +
+                        "    \"locationType\": \"PBP\",\n" +
+                        "    \"locationID\": \"Meter 100-300\",\n" +
+                        "    \"comment\": null,\n" +
+                        "    \"delayReasonCode\": null" +
+                        "}\n").
+                when().post(Configuration.ROOT_URI + "/transport-calls/port-call-events").
+                then().body("$", hasKey("eventID"));
+    }
+
+    @Test
+    public void testPostNewPTAPBP(){
+        given().
+                auth().
+                oauth2(Configuration.accessToken).
+                contentType("application/json").
+                body(   "  {\n" +
+                        "  \"eventType\": \"PORTCALL\",\n" +
+                        "    \"eventDateTime\": \"2021-01-30T20:20:00+02:00\",\n" +
+                        "    \"eventClassifierCode\": \"PLN\",\n" +
+                        "    \"eventTypeCode\": \"ARRI\",\n" +
+                        "    \"transportCallID\": \""+this.queryForTransportCallID()+"\",\n" +
+                        "    \"locationType\": \"PBP\",\n" +
+                        "    \"locationID\": \"Meter 100-300\",\n" +
+                        "    \"comment\": null,\n" +
+                        "    \"delayReasonCode\": null" +
+                        "}\n").
+                when().post(Configuration.ROOT_URI + "/transport-calls/port-call-events").
+                then().body("$", hasKey("eventID"));
+    }
+
+    @Test
+    public void testPostNewATAPBP(){
+        given().
+                auth().
+                oauth2(Configuration.accessToken).
+                contentType("application/json").
+                body(   "  {\n" +
+                        "  \"eventType\": \"PORTCALL\",\n" +
+                        "    \"eventDateTime\": \"2021-01-30T20:20:00+02:00\",\n" +
+                        "    \"eventClassifierCode\": \"ACT\",\n" +
+                        "    \"eventTypeCode\": \"ARRI\",\n" +
+                        "    \"transportCallID\": \""+this.queryForTransportCallID()+"\",\n" +
+                        "    \"locationType\": \"PBP\",\n" +
+                        "    \"locationID\": \"Meter 100-300\",\n" +
+                        "    \"comment\": null,\n" +
+                        "    \"delayReasonCode\": null" +
+                        "}\n").
+                when().post(Configuration.ROOT_URI + "/transport-calls/port-call-events").
+                then().body("$", hasKey("eventID"));
+    }
+
+    @Test
+    public void testPostNewETCCOPS(){
+        given().
+                auth().
+                oauth2(Configuration.accessToken).
+                contentType("application/json").
+                body(   "  {\n" +
+                        "  \"eventType\": \"PORTCALL\",\n" +
+                        "    \"eventDateTime\": \"2021-01-30T20:20:00+02:00\",\n" +
+                        "    \"eventClassifierCode\": \"EST\",\n" +
+                        "    \"eventTypeCode\": \"COPS\",\n" +
+                        "    \"transportCallID\": \""+this.queryForTransportCallID()+"\",\n" +
+                        "    \"locationType\": \"CARGO_OPS\",\n" +
+                        "    \"locationID\": \"Meter 100-300\",\n" +
+                        "    \"comment\": null,\n" +
+                        "    \"delayReasonCode\": null" +
+                        "}\n").
+                when().post(Configuration.ROOT_URI + "/transport-calls/port-call-events").
+                then().body("$", hasKey("eventID"));
+    }
+
+    @Test
+    public void testPostNewATCCOPS(){
+        given().
+                auth().
+                oauth2(Configuration.accessToken).
+                contentType("application/json").
+                body(   "  {\n" +
+                        "  \"eventType\": \"PORTCALL\",\n" +
+                        "    \"eventDateTime\": \"2021-01-30T20:20:00+02:00\",\n" +
+                        "    \"eventClassifierCode\": \"ACT\",\n" +
+                        "    \"eventTypeCode\": \"COPS\",\n" +
+                        "    \"transportCallID\": \""+this.queryForTransportCallID()+"\",\n" +
+                        "    \"locationType\": \"CARGO_OPS\",\n" +
+                        "    \"locationID\": \"Meter 100-300\",\n" +
+                        "    \"comment\": null,\n" +
+                        "    \"delayReasonCode\": null" +
+                        "}\n").
+                when().post(Configuration.ROOT_URI + "/transport-calls/port-call-events").
+                then().body("$", hasKey("eventID"));
+    }
+
+    @Test
+    public void testPostNewETDBerth(){
+        given().
+                auth().
+                oauth2(Configuration.accessToken).
+                contentType("application/json").
+                body(   "  {\n" +
+                        "  \"eventType\": \"PORTCALL\",\n" +
+                        "    \"eventDateTime\": \"2021-01-30T20:20:00+02:00\",\n" +
+                        "    \"eventClassifierCode\": \"EST\",\n" +
+                        "    \"eventTypeCode\": \"DEPT\",\n" +
+                        "    \"transportCallID\": \""+this.queryForTransportCallID()+"\",\n" +
+                        "    \"locationType\": \"BERTH\",\n" +
+                        "    \"locationID\": \"Meter 100-300\",\n" +
+                        "    \"comment\": null,\n" +
+                        "    \"delayReasonCode\": null" +
+                        "}\n").
+                when().post(Configuration.ROOT_URI + "/transport-calls/port-call-events").
+                then().body("$", hasKey("eventID"));
+    }
+
+    @Test
+    public void testPostNewRBerth(){
+        given().
+                auth().
+                oauth2(Configuration.accessToken).
+                contentType("application/json").
+                body(   "  {\n" +
+                        "  \"eventType\": \"PORTCALL\",\n" +
+                        "    \"eventDateTime\": \"2021-01-30T20:20:00+02:00\",\n" +
+                        "    \"eventClassifierCode\": \"REQ\",\n" +
+                        "    \"eventTypeCode\": \"DEPT\",\n" +
+                        "    \"transportCallID\": \""+this.queryForTransportCallID()+"\",\n" +
+                        "    \"locationType\": \"BERTH\",\n" +
+                        "    \"locationID\": \"Meter 100-300\",\n" +
+                        "    \"comment\": null,\n" +
+                        "    \"delayReasonCode\": null" +
+                        "}\n").
+                when().post(Configuration.ROOT_URI + "/transport-calls/port-call-events").
+                then().body("$", hasKey("eventID"));
+    }
+
+    private String queryForTransportCallID() {
+
+        String scheduleID = given().auth().oauth2(Configuration.accessToken).
+                get(Configuration.ROOT_URI + "/transport-calls").body().jsonPath().getList("transportCallID").get(0).toString();
+        Assert.assertNotNull(scheduleID);
+        return scheduleID;
+    }
+
+}
---
+++ src/test/java/org/dcsa/api_validator/ovs/v1/transportcalls/PostTransportCallsTest.java
@@ -0,0 +1,64 @@
+package org.dcsa.api_validator.ovs.v1.transportcalls;
+
+import org.dcsa.api_validator.conf.Configuration;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasKey;
+
+public class PostTransportCallsTest {
+
+
+
+
+
+    @Test
+    public void testPostNewTransportCall(){
+        //@ToDo Change IMO Number to 6409715 after bugfix on core is done
+        String vesselIMONumber = "6409711";
+        String vesselName = "MV Kooringa";
+        String UNLocationCode =  "NLRTM";
+        String UNLocationName = "Rotterdamm";
+        String facilityCode = "NLRTMAPM";
+
+
+        given().
+                auth().
+                oauth2(Configuration.accessToken).
+                contentType("application/json").
+                body(" \n" +
+                        "  {\n" +
+                        "    \"scheduleID\": \"" + queryForScheduleID() + "\",\n" +
+                        "    \"carrierServiceCode\": \"EXP2\",\n" +
+                        "    \"vesselIMONumber\": \""+vesselIMONumber+ "\",\n" +
+                        "    \"vesselName\": \""+vesselName+ "\",\n" +
+                        "    \"carrierVoyageNumber\": \"2015W\",\n" +
+                        "    \"UNLocationCode\": \""+UNLocationCode+"\",\n" +
+                        "    \"UNLocationName\": \""+UNLocationName+"\",\n" +
+                        "    \"transportCallNumber\": 2,\n" +
+                        "    \"facilityTypeCode\": \"POTE\",\n" +
+                        "    \"facilityCode\": \""+facilityCode+"\",\n" +
+                        "    \"otherFacility\": \"\"\n" +
+                        "  }\n" +
+                        "").
+                when().
+                post(Configuration.ROOT_URI + "/transport-calls").
+                then().body("vesselIMONumber", equalTo(vesselIMONumber)).
+                body("vesselName", equalTo(vesselName)).
+                body("UNLocationCode", equalTo(UNLocationCode)).
+                body("UNLocationName", equalTo(UNLocationName)).
+                body("facilityCode", equalTo(facilityCode)).
+                body("$", hasKey("transportCallID"));
+
+        }
+
+
+    private String queryForScheduleID() {
+
+        String scheduleID = given().auth().oauth2(Configuration.accessToken).
+                get(Configuration.ROOT_URI + "/schedules").body().jsonPath().getList("scheduleID").get(0).toString();
+        Assert.assertNotNull(scheduleID);
+        return scheduleID;
+    }
+}

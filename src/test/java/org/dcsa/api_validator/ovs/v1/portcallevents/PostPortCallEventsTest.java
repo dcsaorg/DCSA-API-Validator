7ea74dec33efd125be3fd7906085cba20ff395d8
holmHin <holm.hinrichs@t-online.de>
2021-02-10 18:25:42 +0100

    Added PortCall Get Test

--- src/test/java/org/dcsa/api_validator/ovs/v1/portcallevents/PostPortCallEventsTest.java
+++ src/test/java/org/dcsa/api_validator/ovs/v1/portcallevents/PostPortCallEventsTest.java
@@ -7,7 +7,7 @@ import org.testng.annotations.Test;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.hasKey;
 
-public class PostPortCallEvents {
+public class PostPortCallEventsTest {
 
     @Test
     public void testPostNewETABerth(){

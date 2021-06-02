ca4959888ef1673b84e162f1e9bad0c5a78b5d03
Niels Thykier <nt@asseco.dk>
2021-06-02 11:15:29 +0200

    Update to reflect recent changes
    Signed-off-by: Niels Thykier <nt@asseco.dk>

--- src/test/java/org/dcsa/api_validator/tnt/v2/GetEventsIDTest.java
+++ src/test/java/org/dcsa/api_validator/tnt/v2/GetEventsIDTest.java
@@ -13,6 +13,7 @@ import java.util.List;
 import static com.github.fge.jsonschema.SchemaVersion.DRAFTV4;
 import static io.restassured.RestAssured.given;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
 import static org.hamcrest.Matchers.is;
 
 /*
@@ -27,45 +28,13 @@ public class GetEventsIDTest {
     @Test
     public void testEventIds() {
         Reporter.log("Running testEventIds ------------------------------------------------------------------------------------------------------------------------");
-        String json = given().
+        given().
                 auth().
                 oauth2(Configuration.accessToken).
                 get(Configuration.ROOT_URI + "/events").
-                body().asString();
+                then().assertThat().statusCode(200).
+                assertThat().body("size()", greaterThanOrEqualTo(0)).
+                assertThat().body(matchesJsonSchemaInClasspath("tnt/v2/EventsSchema.json"));
 
-        List<String> ids = JsonPath.from(json).getList("eventID");
-        for (String id : ids) {
-            given().
-                    auth().
-                    oauth2(Configuration.accessToken).
-                    get(Configuration.ROOT_URI + "/events/" + id).
-                    then().
-                    body("eventID", is(id)).
-                    assertThat().body(matchesJsonSchemaInClasspath("tnt/v2/EventSchema.json").using(jsonSchemaFactory));
-        }
-
-    }
-
-    //Ensures a bad request is thrown when the path parameter is not an UUID. If API implementers don't use UUIDs, then this test should be removed
-    @Test
-    public void testIncorrectUUID() {
-        given().
-                auth().
-                oauth2(Configuration.accessToken).
-        get(Configuration.ROOT_URI+"/events/NOT-AN-UUID")
-                .then()
-                .assertThat().
-                statusCode(HttpStatus.SC_BAD_REQUEST);
-    }
-
-    @Test
-    public void testNotFoundUUID() {
-        given().
-                auth().
-                oauth2(Configuration.accessToken).
-        get(Configuration.ROOT_URI+"/events/80d63706-7b93-4936-84fe-3ef9ef1946f0")
-                .then()
-                .assertThat().
-                statusCode(HttpStatus.SC_NOT_FOUND);
     }
 }

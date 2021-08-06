2d829528f19740b62a52524844e187d6877a70d4
abf <abf@asseco.dk>
2021-08-06 11:39:07 +0200

    Minor fixes
    - Renaming methods in testsuite
    - Fixing latitude & longtitude in sample file
    - added a test for portCallServiceTypeCode enum.

--- src/test/java/org/dcsa/api_validator/ovs/v2/PostTimestampsTest.java
+++ src/test/java/org/dcsa/api_validator/ovs/v2/PostTimestampsTest.java
@@ -20,7 +20,24 @@ public class PostTimestampsTest {
 
     public static final String VALID_TIMESTAMP = loadFileAsString("ovs/v2/ovsTimeStamps/TimeStampsSample.json");
 
-    @Test
+    // Testing with all fields provided in VALID_TIMESTAMP variable
+    @Test(enabled = false)
+    public void testTimestampRequiredParameters() {
+        given().
+                auth().
+                oauth2(Configuration.accessToken).
+                contentType("application/json").
+                header("testname","testTimestampRequiredParameters").
+                body(VALID_TIMESTAMP).
+                post(Configuration.ROOT_URI + "/timestamps").
+                then().
+                assertThat().
+                statusCode(204);
+    }
+
+    // Testing with no fields / Empty body
+    // Should fail as nothing is provided
+    @Test(enabled = false)
     public void testTransportCallsNoMandatoryParameters() {
         given().
                 auth().
@@ -37,30 +54,17 @@ public class PostTimestampsTest {
                 oauth2(Configuration.accessToken).
                 contentType("application/json").
                 header("testname","testTransportCallsNoMandatoryParameters2").
-                body("").
+                body("").                                                       // Note: Empty body
                 post(Configuration.ROOT_URI + "/timestamps").
                 then().
                 assertThat().
                 statusCode(400);
     }
 
-    // These are mandatory parameters and is the main test
-    @Test
-    public void testTimestampRequiredParameters() {
-        given().
-                auth().
-                oauth2(Configuration.accessToken).
-                contentType("application/json").
-                header("testname","testTimestampRequiredParameters").
-                body(VALID_TIMESTAMP).
-                post(Configuration.ROOT_URI + "/timestamps").
-                then().
-                assertThat().
-                statusCode(204);
-    }
 
+    // Testing with mandatory fields + FacilitySMDGCode field
     @Test
-    public void testFacilitySMDGCodeQueryParam() {
+    public void testFacilitySMDGCodeField() {
 
         Map<String, String> map = (Map<String, String>) jsonToMap(VALID_TIMESTAMP);
         map.remove("eventLocation");
@@ -72,7 +76,7 @@ public class PostTimestampsTest {
                 auth().
                 oauth2(Configuration.accessToken).
                 contentType("application/json").
-                header("testname","testFacilitySMDGCodeQueryParam").
+                header("testname","testFacilitySMDGCodeField").
                 body(map).
                 post(Configuration.ROOT_URI + "/timestamps").
                 then().
@@ -80,15 +84,17 @@ public class PostTimestampsTest {
                 statusCode(204);
     }
 
-    // Test facilitySMDGCode, fails as specification -> maxLength:6 & ENUM
-    @Test
-    public void testFacilitySMDGCodeFalseFormat() {
+    // Test // Testing with mandatory fields + FacilitySMDGCode field
+    // Should fail because specification -> maxLength:6
+    @Test(enabled = false)
+    public void testFacilitySMDGCodeFieldFalseFormat() {
 
         Map<String, String> map = (Map<String, String>) jsonToMap(VALID_TIMESTAMP);
         map.remove("eventLocation");
         map.remove("vesselPosition");
         map.remove("modeOfTransport");
         map.remove("portCallServiceTypeCode");
+
         map.put("facilitySMDGCode","aBCDaFGHaE");
 
         given().
@@ -100,22 +106,11 @@ public class PostTimestampsTest {
                 then().
                 assertThat().
                 statusCode(400);
-
-        map.put("facilitySMDGCode", "aBC");
-
-        given().
-                auth().
-                oauth2(Configuration.accessToken).
-                contentType("application/json").
-                body(map).
-                post(Configuration.ROOT_URI + "/timestamps").
-                then().
-                assertThat().
-                statusCode(400);
     }
 
-    @Test
-    public void testEventLocationQueryParam() {
+    // Testing with mandatory fields + EventLocation field
+    @Test(enabled = false)
+    public void testEventLocationField() {
 
         Map<String, String> map = (Map<String, String>) jsonToMap(VALID_TIMESTAMP);
         map.remove("facilitySMDGCode");
@@ -127,7 +122,7 @@ public class PostTimestampsTest {
                 auth().
                 oauth2(Configuration.accessToken).
                 contentType("application/json").
-                header("testname","testEventLocationQueryParam").
+                header("testname","testEventLocationField").
                 body(map).
                 post(Configuration.ROOT_URI + "/timestamps").
                 then().
@@ -135,8 +130,9 @@ public class PostTimestampsTest {
                 statusCode(204);
     }
 
-    // Test EventLocation, fails as format is JSON.
-    @Test
+    // Testing with mandatory fields + EventLocation field
+    // fails as eventLocation is an object.
+    @Test(enabled = false)
     public void testEventLocationFalseFormat() {
 
         Map<String, String> map = (Map<String, String>) jsonToMap(VALID_TIMESTAMP);
@@ -145,7 +141,7 @@ public class PostTimestampsTest {
         map.remove("modeOfTransport");
         map.remove("portCallServiceTypeCode");
 
-        map.put("eventLocation","aBCDaFGHaE"); // Wrong format
+        map.put("eventLocation","aBCDaFGHaE"); // Wrong format - eventLocation is an object
 
         given().
                 auth().
@@ -157,21 +153,11 @@ public class PostTimestampsTest {
                 assertThat().
                 statusCode(400);
 
-        map.put("eventLocation", "{}"); // Empty (fails Maybe - does not include mandatory parameters)
-
-        given().
-                auth().
-                oauth2(Configuration.accessToken).
-                contentType("application/json").
-                body(map).
-                post(Configuration.ROOT_URI + "/timestamps").
-                then().
-                assertThat().
-                statusCode(400);
     }
 
-    @Test
-    public void testVesselPositionQueryParam() {
+    // Testing with mandatory fields + VesselPosition field
+    @Test(enabled = false)
+    public void testVesselPositionField() {
 
         Map<String, String> map = (Map<String, String>) jsonToMap(VALID_TIMESTAMP);
         map.remove("facilitySMDGCode");
@@ -183,7 +169,7 @@ public class PostTimestampsTest {
                 auth().
                 oauth2(Configuration.accessToken).
                 contentType("application/json").
-                header("testname","testVesselPositionQueryParam").
+                header("testname","testVesselPositionField").
                 body(map).
                 post(Configuration.ROOT_URI + "/timestamps").
                 then().
@@ -191,8 +177,9 @@ public class PostTimestampsTest {
                 statusCode(204);
     }
 
-    // Test vesselPosition, fails as as format is JSON & (latitude & longitude required).
-    @Test
+    // Testing with mandatory fields + vesselPosition field
+    // fails as vesselPosition is an object & (latitude & longitude are required parameters).
+    @Test(enabled = false)
     public void testVesselPositionFalseFormat() {
         Map<String, String> map = (Map<String, String>) jsonToMap(VALID_TIMESTAMP);
         map.remove("facilitySMDGCode");
@@ -212,7 +199,7 @@ public class PostTimestampsTest {
                 assertThat().
                 statusCode(400);
 
-        map.put("vesselPosition", "{}"); // Empty (fails - includes mandatory parameters)
+        map.put("vesselPosition", "{}"); // Empty (fails - vesselPosition includes mandatory parameters)
 
         given().
                 auth().
@@ -225,8 +212,9 @@ public class PostTimestampsTest {
                 statusCode(400);
     }
 
-    @Test
-    public void testModeOfTransportQueryParam() {
+    // Testing with mandatory fields + ModeOfTransport field
+    @Test(enabled = false)
+    public void testModeOfTransportField() {
 
         Map<String, String> map = (Map<String, String>) jsonToMap(VALID_TIMESTAMP);
         map.remove("facilitySMDGCode");
@@ -238,7 +226,7 @@ public class PostTimestampsTest {
                 auth().
                 oauth2(Configuration.accessToken).
                 contentType("application/json").
-                header("testname","testModeOfTransportQueryParam").
+                header("testname","testModeOfTransportField").
                 body(map).
                 post(Configuration.ROOT_URI + "/timestamps").
                 then().
@@ -246,16 +234,17 @@ public class PostTimestampsTest {
                 statusCode(204);
     }
 
-    // Test modeOfTransport, fails as VES is not ENUM.
-    @Test
-    public void testModeOfTransportFalseFormat() {
+    // Testing with mandatory fields + ModeOfTransport field
+    // fails as ModeOfTransport is an ENUM.
+    @Test(enabled = false)
+    public void testModeOfTransportFieldFalseFormat() {
         Map<String, String> map = (Map<String, String>) jsonToMap(VALID_TIMESTAMP);
         map.remove("facilitySMDGCode");
         map.remove("modeOfTransport");
         map.remove("eventLocation");
         map.remove("portCallServiceTypeCode");
 
-        map.put("vesselPosition","aBCDaFGHaE"); // Wrong format
+        map.put("modeOfTransport","VES"); // Wrong format ( VES -- is not ENUM)
 
         given().
                 auth().
@@ -267,7 +256,7 @@ public class PostTimestampsTest {
                 assertThat().
                 statusCode(400);
 
-        map.put("vesselPosition", "{}"); // Empty (fails - includes mandatory parameters)
+        map.put("modeOfTransport", ""); // Empty (fails - not ENUM)
 
         given().
                 auth().
@@ -280,8 +269,67 @@ public class PostTimestampsTest {
                 statusCode(400);
     }
 
-    @Test
-    public void testMandatoryPublisherQueryParamFalseFormat() {
+    // Testing with mandatory fields + PortCallServiceTypeCode field
+    @Test(enabled = false)
+    public void testPortCallServiceTypeCodeField() {
+
+        Map<String, String> map = (Map<String, String>) jsonToMap(VALID_TIMESTAMP);
+        map.remove("facilitySMDGCode");
+        map.remove("vesselPosition");
+        map.remove("eventLocation");
+        map.remove("modeOfTransport");
+
+        given().
+                auth().
+                oauth2(Configuration.accessToken).
+                contentType("application/json").
+                header("testname","testPortCallServiceTypeCodeField").
+                body(map).
+                post(Configuration.ROOT_URI + "/timestamps").
+                then().
+                assertThat().
+                statusCode(204);
+    }
+    // Testing with mandatory fields + PortCallServiceTypeCode field
+    // fails as portCallServiceTypeCode is an ENUM.
+    // Test modeOfTransport,
+    @Test(enabled = false)
+    public void testPortCallServiceTypeCodeFalseFormat() {
+        Map<String, String> map = (Map<String, String>) jsonToMap(VALID_TIMESTAMP);
+        map.remove("facilitySMDGCode");
+        map.remove("modeOfTransport");
+        map.remove("eventLocation");
+        map.remove("modeOfTransport");
+
+        map.put("portCallServiceTypeCode","VES"); // Wrong format ( VES -- is not ENUM)
+
+        given().
+                auth().
+                oauth2(Configuration.accessToken).
+                contentType("application/json").
+                body(map).
+                post(Configuration.ROOT_URI + "/timestamps").
+                then().
+                assertThat().
+                statusCode(400);
+
+        map.put("portCallServiceTypeCode", ""); // Empty (fails - not ENUM)
+
+        given().
+                auth().
+                oauth2(Configuration.accessToken).
+                contentType("application/json").
+                body(map).
+                post(Configuration.ROOT_URI + "/timestamps").
+                then().
+                assertThat().
+                statusCode(400);
+    }
+
+    // Testing with mandatory fields - Except PublisherField field
+    // Should fail as PublisherField is mandatory
+    @Test(enabled = false)
+    public void testMandatoryPublisherFieldFalseFormat() {
 
         Map<String, String> map = (Map<String, String>) jsonToMap(VALID_TIMESTAMP);
 
@@ -309,7 +357,7 @@ public class PostTimestampsTest {
                 statusCode(400);
 
         // Empty Publisher parameter
-        map.put("publisher", "{}"); // Empty (fails Maybe - does not include mandatory parameters)
+        map.put("publisher", "{}"); // Empty
         given().
                 auth().
                 oauth2(Configuration.accessToken).
@@ -321,7 +369,7 @@ public class PostTimestampsTest {
                 statusCode(400);
 
         // Wrong format
-        map.put("publisher", ""); // Empty (fails Maybe - does not include mandatory parameters)
+        map.put("publisher", ""); // Empty
         given().
                 auth().
                 oauth2(Configuration.accessToken).
@@ -333,8 +381,10 @@ public class PostTimestampsTest {
                 statusCode(400);
     }
 
-    @Test
-    public void testMandatoryPublisherRoleQueryParamFalseFormat() {
+    // Testing with mandatory fields - Except publisherRole field
+    // Should fail as publisherRole is mandatory
+    @Test(enabled = false)
+    public void testMandatoryPublisherRoleFieldFalseFormat() {
         Map<String, String> map = (Map<String, String>) jsonToMap(VALID_TIMESTAMP);
 
         map.remove("publisherRole");
@@ -361,9 +411,10 @@ public class PostTimestampsTest {
                 statusCode(400);
     }
 
-    // Missing mandatory VesselIMONumber parameter
-    @Test
-    public void testMandatoryVesselIMONumberQueryParamFalseFormat() {
+    // Testing with mandatory fields - Except vesselIMONumber field
+    // Should fail as vesselIMONumber is mandatory
+    @Test(enabled = false)
+    public void testMandatoryVesselIMONumberFieldFalseFormat() {
 
         Map<String, String> map = (Map<String, String>) jsonToMap(VALID_TIMESTAMP);
 
@@ -403,8 +454,10 @@ public class PostTimestampsTest {
                 statusCode(400);
     }
 
-    @Test
-    public void testMandatoryUNLocationCodeQueryParamFalseFormat() {
+    // Testing with mandatory fields - Except UNLocationCode field
+    // Should fail as UNLocationCode is mandatory
+    @Test(enabled = false)
+    public void testMandatoryUNLocationCodeFieldFalseFormat() {
         Map<String, String> map = (Map<String, String>) jsonToMap(VALID_TIMESTAMP);
 
         map.remove("UNLocationCode");
@@ -443,8 +496,10 @@ public class PostTimestampsTest {
                 statusCode(400);
     }
 
-    @Test
-    public void testMandatoryFacilityTypeCodeQueryParamFalseFormat() {
+    // Testing with mandatory fields - Except FacilityTypeCode field
+    // Should fail as FacilityTypeCode is mandatory
+    @Test(enabled = false)
+    public void testMandatoryFacilityTypeCodeFieldFalseFormat() {
 
         Map<String, String> map = (Map<String, String>) jsonToMap(VALID_TIMESTAMP);
 
@@ -484,9 +539,10 @@ public class PostTimestampsTest {
                 statusCode(400);
     }
 
-    // Missing mandatory EventClassifierCode parameter
-    @Test
-    public void testMandatoryEventClassifierCodeQueryParamFalseFormat() {
+    // Testing with mandatory fields - Except EventClassifierCode field
+    // Should fail as EventClassifierCode is mandatory
+    @Test(enabled = false)
+    public void testMandatoryEventClassifierCodeFieldFalseFormat() {
 
         Map<String, String> map = (Map<String, String>) jsonToMap(VALID_TIMESTAMP);
 
@@ -514,9 +570,10 @@ public class PostTimestampsTest {
                 statusCode(400);
     }
 
-    // Missing mandatory OperationsEventTypeCode parameter
-    @Test
-    public void testMandatoryOperationsEventTypeCodeQueryParamFalseFormat() {
+    // Testing with mandatory fields - Except OperationsEventTypeCode field
+    // Should fail as OperationsEventTypeCode is mandatory
+    @Test(enabled = false)
+    public void testMandatoryOperationsEventTypeCodeFieldFalseFormat() {
 
         Map<String, String> map = (Map<String, String>) jsonToMap(VALID_TIMESTAMP);
 
@@ -544,9 +601,10 @@ public class PostTimestampsTest {
                 statusCode(400);
     }
 
-    // Missing mandatory EventDateTime parameter
-    @Test
-    public void testMandatoryEventDateTimeQueryParamFalseFormat() {
+    // Testing with mandatory fields - Except EventDateTime field
+    // Should fail as EventDateTime is mandatory
+    @Test(enabled = false)
+    public void testMandatoryEventDateTimeFieldFalseFormat() {
 
         Map<String, String> map = (Map<String, String>) jsonToMap(VALID_TIMESTAMP);
 
@@ -562,7 +620,7 @@ public class PostTimestampsTest {
                 statusCode(400);
 
         // Wrong value
-        map.put("eventDateTime", "sdf"); // Empty
+        map.put("eventDateTime", "sdf"); // wrong format
         given().
                 auth().
                 oauth2(Configuration.accessToken).

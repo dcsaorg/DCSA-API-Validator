ee20ce16a85ae3ec5d6a8d96e0ff07a9e530eff5
abf <abf@asseco.dk>
2022-01-05 14:13:23 +0100

    Tests for GET /shipments/{carrierBookingReference}

--- src/test/java/org/dcsa/api_validator/bkg/v1/BookingTestConfiguration.java
+++ src/test/java/org/dcsa/api_validator/bkg/v1/BookingTestConfiguration.java
@@ -9,6 +9,7 @@ import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInC
 
 public class BookingTestConfiguration {
 
+  public static final String SHIPMENT_PATH = "/shipments/{carrierBookingReference}";
   public static final String SHIPMENT_SUMMARIES_PATH = "/shipment-summaries";
   public static final String BOOKING_SUMMARIES_PATH = "/booking-summaries";
   public static final String BOOKING_CARRIERBOOKINGREQUESTREFERENCE_PATH = "/bookings/{carrierBookingRequestReference}";

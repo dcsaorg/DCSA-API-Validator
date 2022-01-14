ba56375ed05bb9b5fffb459fdf00dd2dc26976a4
Martin Petersson <mp@asseco.dk>
2022-01-14 10:06:14 +0100

    Update PostBookingTest.java

--- src/test/java/org/dcsa/api_validator/bkg/v1/bookings/PostBookingTest.java
+++ src/test/java/org/dcsa/api_validator/bkg/v1/bookings/PostBookingTest.java
@@ -1,5 +1,6 @@
 package org.dcsa.api_validator.bkg.v1.bookings;
 
+import org.apache.http.HttpStatus;
 import org.dcsa.api_validator.conf.Configuration;
 import org.testng.annotations.Test;
 
@@ -22,94 +23,93 @@ public class PostBookingTest {
 
   @Test
   public void testValidBookingPost() {
-    String splat =
-        given()
-            .contentType("application/json")
-            .body(VALID_BOOKING)
-            .post(Configuration.ROOT_URI + BOOKING_PATH)
-            .then()
-            .assertThat()
-            .body("documentStatus", equalTo("RECE"))
-            .body("carrierBookingRequestReference", notNullValue())
-            .body("bookingRequestCreatedDateTime", notNullValue())
-            .body("bookingRequestUpdatedDateTime", notNullValue())
-            .statusCode(202)
-            .body(JSON_SCHEMA_VALIDATOR)
-            .extract()
-            .body()
-            .asString();
+    given()
+        .contentType("application/json")
+        .body(VALID_BOOKING)
+        .post(Configuration.ROOT_URI + BOOKING_PATH)
+        .then()
+        .assertThat()
+        .body("documentStatus", equalTo("RECE"))
+        .body("carrierBookingRequestReference", notNullValue())
+        .body("bookingRequestCreatedDateTime", notNullValue())
+        .body("bookingRequestUpdatedDateTime", notNullValue())
+        .statusCode(HttpStatus.SC_OK)
+        .body(JSON_SCHEMA_VALIDATOR)
+        .extract()
+        .body()
+        .asString();
   }
 
   @Test
   public void testBookingPostMissingAllMandatoryFields() {
-      Map<String, Object> map = jsonToMap(VALID_BOOKING);
-      assert map != null;
-      map.remove("receiptTypeAtOrigin");
-      map.remove("deliveryTypeAtDestination");
-      map.remove("cargoMovementTypeAtOrigin");
-      map.remove("cargoMovementTypeAtDestination");
-      map.remove("serviceContractReference");
-      map.remove("isPartialLoadAllowed");
-      map.remove("isExportDeclarationRequired");
-      map.remove("isImportLicenseRequired");
-      map.remove("submissionDateTime");
-      map.remove("communicationChannel");
-      map.remove("isEquipmentSubstitutionAllowed");
-      map.remove("commodities");
+    Map<String, Object> map = jsonToMap(VALID_BOOKING);
+    assert map != null;
+    map.remove("receiptTypeAtOrigin");
+    map.remove("deliveryTypeAtDestination");
+    map.remove("cargoMovementTypeAtOrigin");
+    map.remove("cargoMovementTypeAtDestination");
+    map.remove("serviceContractReference");
+    map.remove("isPartialLoadAllowed");
+    map.remove("isExportDeclarationRequired");
+    map.remove("isImportLicenseRequired");
+    map.remove("submissionDateTime");
+    map.remove("communicationChannel");
+    map.remove("isEquipmentSubstitutionAllowed");
+    map.remove("commodities");
 
-      given()
-          .auth()
-          .oauth2(Configuration.accessToken)
-          .contentType("application/json")
-          .body(map)
-          .post(Configuration.ROOT_URI + BOOKING_PATH)
-          .then()
-          .assertThat()
-          .statusCode(400)
-          .body(JSON_SCHEMA_VALIDATOR);
+    given()
+        .auth()
+        .oauth2(Configuration.accessToken)
+        .contentType("application/json")
+        .body(map)
+        .post(Configuration.ROOT_URI + BOOKING_PATH)
+        .then()
+        .assertThat()
+        .statusCode(HttpStatus.SC_BAD_REQUEST)
+        .body(JSON_SCHEMA_VALIDATOR);
   }
 
   @Test
   public void testBookingPostMissingAllOptionalFields() {
-      Map<String, Object> map = jsonToMap(VALID_BOOKING);
-      assert map != null;
-      map.remove("paymentTermCode");
-      map.remove("exportDeclarationReference");
-      map.remove("importLicenseReference");
-      map.remove("isAMSACIFilingRequired");
-      map.remove("isDestinationFilingRequired");
-      map.remove("contractQuotationReference");
-      map.remove("expectedDepartureDate");
-      map.remove("transportDocumentTypeCode");
-      map.remove("transportDocumentReference");
-      map.remove("bookingChannelReference");
-      map.remove("incoTerms");
-      map.remove("vesselName");
-      map.remove("vesselIMONumber");
-      map.remove("exportVoyageNumber");
-      map.remove("preCarriageModeOfTransportCode");
-      map.remove("invoicePayableAt");
-      map.remove("placeOfIssue");
-      map.remove("valueAddedServiceRequests");
-      map.remove("references");
-      map.remove("requestedEquipments");
-      map.remove("documentParties");
-      map.remove("shipmentLocations");
+    Map<String, Object> map = jsonToMap(VALID_BOOKING);
+    assert map != null;
+    map.remove("paymentTermCode");
+    map.remove("exportDeclarationReference");
+    map.remove("importLicenseReference");
+    map.remove("isAMSACIFilingRequired");
+    map.remove("isDestinationFilingRequired");
+    map.remove("contractQuotationReference");
+    map.remove("expectedDepartureDate");
+    map.remove("transportDocumentTypeCode");
+    map.remove("transportDocumentReference");
+    map.remove("bookingChannelReference");
+    map.remove("incoTerms");
+    map.remove("vesselName");
+    map.remove("vesselIMONumber");
+    map.remove("exportVoyageNumber");
+    map.remove("preCarriageModeOfTransportCode");
+    map.remove("invoicePayableAt");
+    map.remove("placeOfIssue");
+    map.remove("valueAddedServiceRequests");
+    map.remove("references");
+    map.remove("requestedEquipments");
+    map.remove("documentParties");
+    map.remove("shipmentLocations");
 
-      given()
-          .auth()
-          .oauth2(Configuration.accessToken)
-          .contentType("application/json")
-          .body(map)
-          .post(Configuration.ROOT_URI + BOOKING_PATH)
-          .then()
-          .assertThat()
-          .body("documentStatus", equalTo("RECE"))
-          .body("carrierBookingRequestReference", notNullValue())
-          .body("bookingRequestCreatedDateTime", notNullValue())
-          .body("bookingRequestUpdatedDateTime", notNullValue())
-          .statusCode(202)
-          .body(JSON_SCHEMA_VALIDATOR);
+    given()
+        .auth()
+        .oauth2(Configuration.accessToken)
+        .contentType("application/json")
+        .body(map)
+        .post(Configuration.ROOT_URI + BOOKING_PATH)
+        .then()
+        .assertThat()
+        .body("documentStatus", equalTo("RECE"))
+        .body("carrierBookingRequestReference", notNullValue())
+        .body("bookingRequestCreatedDateTime", notNullValue())
+        .body("bookingRequestUpdatedDateTime", notNullValue())
+        .statusCode(HttpStatus.SC_ACCEPTED)
+        .body(JSON_SCHEMA_VALIDATOR);
   }
 
   // Test case with all optional fields removed
@@ -119,17 +119,17 @@ public class PostBookingTest {
 
     Map<String, Object> map = jsonToMap(VALID_BOOKING);
 
-    given().
-            auth().
-            oauth2(Configuration.accessToken).
-            contentType("application/json").
-            body(map).
-            post(Configuration.ROOT_URI + BOOKING_PATH).
-            then().
-            assertThat()
-            .header("API-Version", "1.0.0").
-            statusCode(202).
-            body("size()", greaterThanOrEqualTo(0)).
-            body(JSON_SCHEMA_VALIDATOR);
+    given()
+        .auth()
+        .oauth2(Configuration.accessToken)
+        .contentType("application/json")
+        .body(map)
+        .post(Configuration.ROOT_URI + BOOKING_PATH)
+        .then()
+        .assertThat()
+        .header("API-Version", "1.0.0")
+        .statusCode(HttpStatus.SC_ACCEPTED)
+        .body("size()", greaterThanOrEqualTo(0))
+        .body(JSON_SCHEMA_VALIDATOR);
   }
 }
